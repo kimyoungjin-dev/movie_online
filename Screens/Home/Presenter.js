aea2818a50477ae406a81e91054f4a7d6cafb47d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-01 18:44:27 +0900

    Render movie information on the home screen.

--- Screens/Home/Presenter.js
+++ Screens/Home/Presenter.js
@@ -1,18 +1,50 @@
 import React from "react";
-import { Text } from "react-native";
+import { ActivityIndicator, Dimensions, ScrollView, View } from "react-native";
 import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import Slide from "../../Components/Slide";
 
-const Container = styled.View`
-  background-color: black;
-  height: 100%;
+const { height: HEIGHT } = Dimensions.get("window");
+
+const SwiperContainer = styled.View`
+  height: ${HEIGHT / 2.5};
   width: 100%;
-  flex: 1;
 `;
-const Presenter = () => {
+
+const Presenter = ({
+  loading,
+  movieNowPlaying,
+  moviePopular,
+  movieTopRated,
+  movieUpcoming,
+}) => {
   return (
-    <Container>
-      <Text>hello</Text>
-    </Container>
+    <ScrollView
+      contentContainerStyle={{
+        flex: loading ? 1 : "auto",
+        justifyContent: loading ? "center" : "auto",
+      }}
+    >
+      {loading ? (
+        <ActivityIndicator color="white" size="large" />
+      ) : (
+        <SwiperContainer>
+          <Swiper loop timeout={3} controlsEnabled={false}>
+            {movieNowPlaying.map((movie) => (
+              <Slide
+                key={movie.id}
+                id={movie.id}
+                adult={movie.adult}
+                poster={movie.poster_path}
+                language={movie.original_language}
+                title={movie.original_title}
+                genre={movie.genre_ids}
+              />
+            ))}
+          </Swiper>
+        </SwiperContainer>
+      )}
+    </ScrollView>
   );
 };
 
