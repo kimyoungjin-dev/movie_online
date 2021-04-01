a8c4fb3cdd2e5e9c0980187ec5aea386cc267b12
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:43:01 +0900

    Receives api information about the movie genre and passes the information to the presenter.

--- Screens/Movie/Container.js
+++ Screens/Movie/Container.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Presenter from "../Movie/Presenter";
+import { MovieApi } from "../../Components/api";
 
 const Container = () => {
-  return <></>;
+  const [genres, setGenres] = useState({
+    Comedy: [],
+    Fantasy: [],
+    Horror: [],
+    Romance: [],
+    Thriller: [],
+    Action: [],
+    Adventure: [],
+    Animation: [],
+    Crime: [],
+    War: [],
+  });
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    const [Comedy] = await MovieApi.MovieDisCovery(35);
+    const [Fantasy] = await MovieApi.MovieDisCovery(14);
+    const [Horror] = await MovieApi.MovieDisCovery(27);
+    const [Romance] = await MovieApi.MovieDisCovery(10749);
+    const [Thriller] = await MovieApi.MovieDisCovery(53);
+    const [Action] = await MovieApi.MovieDisCovery(28);
+    const [Adventure] = await MovieApi.MovieDisCovery(12);
+    const [Animation] = await MovieApi.MovieDisCovery(16);
+    const [Crime] = await MovieApi.MovieDisCovery(80);
+    const [War] = await MovieApi.MovieDisCovery(10752);
+
+    setGenres({
+      Comedy,
+      Fantasy,
+      Horror,
+      Romance,
+      Thriller,
+      Action,
+      Adventure,
+      Animation,
+      Crime,
+      War,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <Presenter {...genres} loading={loading} />;
 };
 
 export default Container;
