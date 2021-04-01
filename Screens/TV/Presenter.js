2f639f21902d4bbe72eeb1912fb77639a5d6afca
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 02:52:24 +0900

    Use the ScrollContainer component.

--- Screens/TV/Presenter.js
+++ Screens/TV/Presenter.js
@@ -3,6 +3,7 @@ import styled from "styled-components/native";
 import { ActivityIndicator, ScrollView } from "react-native";
 import VerticalScroll from "../../Components/SlideComponent/VerticalScroll";
 import Vertical from "../../Components/SlideComponent/Vertical";
+import ScrollContainer from "../../Components/SlideComponent/ScrollContainer";
 
 const Container = styled.View`
   padding: 0px 15px;
@@ -24,168 +25,163 @@ const Presenter = ({
   loading,
 }) => {
   return (
-    <ScrollView
-      style={{ backgroundColor: "black" }}
-      contentContainerStyle={{
-        flex: loading ? 1 : "auto",
-        justifyContent: loading ? "center" : "auto",
-      }}
-    >
-      {loading ? (
-        <ActivityIndicator color="white" size="large" />
-      ) : (
-        <Container>
-          <VerticalScroll title={"ActionAdventure"}>
-            {ActionAdventure.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"애니메이션"}>
-            {Animation.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"Comedy 프로그램"}>
-            {Comedy.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"Crime"}>
-            {Crime.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"Documentary"}>
-            {Documentary.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"Drama"}>
-            {Drama.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"Mystery"}>
-            {Mystery.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-          <VerticalScroll title={"News"}>
-            {News.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
+    <ScrollContainer>
+      <Container>
+        <VerticalScroll title={"ActionAdventure"}>
+          {ActionAdventure.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+        <VerticalScroll title={"애니메이션"}>
+          {Animation.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
 
-          <VerticalScroll title={"Reality"}>
-            {Reality.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
+        <VerticalScroll title={"Comedy 프로그램"}>
+          {Comedy.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
 
-          <VerticalScroll title={"SF_Fantasy"}>
-            {SF_Fantasy.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
+        <VerticalScroll title={"Crime"}>
+          {Crime.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
 
-          <VerticalScroll title={"Talk"}>
-            {Talk.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
+        <VerticalScroll title={"Documentary"}>
+          {Documentary.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
 
-          <VerticalScroll title={"Western"}>
-            {Western.map((show) => (
-              <Vertical
-                key={show.id}
-                id={show.id}
-                title={show.original_name}
-                overview={show.overview}
-                poster={show.poster_path}
-                isShow={true}
-              />
-            ))}
-          </VerticalScroll>
-        </Container>
-      )}
-    </ScrollView>
+        <VerticalScroll title={"Drama"}>
+          {Drama.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Mystery"}>
+          {Mystery.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+        <VerticalScroll title={"News"}>
+          {News.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Reality"}>
+          {Reality.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"SF_Fantasy"}>
+          {SF_Fantasy.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Talk"}>
+          {Talk.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Western"}>
+          {Western.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+              isShow={true}
+            />
+          ))}
+        </VerticalScroll>
+      </Container>
+    </ScrollContainer>
   );
 };
 
