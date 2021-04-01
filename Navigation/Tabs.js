470241068a141d25bd4af5276ec88d820294c7d0
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-01 18:42:49 +0900

    Add folder and user components, and use useLayoutEffect to change the title of tabs to match the router name.

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../Screens/Home";
-import Movie from "../Screens/Movie";
-import TV from "../Screens/TV";
 import Search from "../Screens/Search";
+import Folder from "../Screens/Folder";
+import User from "../Screens/User";
+import { MaterialIcons, Feather } from "@expo/vector-icons";
+import styled from "styled-components/native";
+
+const Container = styled.View``;
 
 const Tabs = createBottomTabNavigator();
 
-export default () => {
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route?.state?.routeNames[route?.state?.index] || "MovieOnline",
+    });
+  }, [route]);
+
   return (
     <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = "";
+          if (route.name === "Home") {
+            iconName = "house";
+          } else if (route.name === "Search") {
+            iconName = "search";
+          } else if (route.name === "Folder") {
+            iconName = "folder-open";
+          } else if (route.name === "User") {
+            return (
+              <Feather
+                name="user"
+                size={30}
+                color={focused ? "white" : "gray"}
+              />
+            );
+          }
+          return (
+            <MaterialIcons
+              name={iconName}
+              size={30}
+              color={focused ? "white" : "gray"}
+            />
+          );
+        },
+      })}
       tabBarOptions={{
         labelStyle: { fontSize: 14 },
-        activeTintColor: "white",
-        inactiveTintColor: "gray",
-        style: { backgroundColor: "black", borderTopColor: "black" },
+        style: { backgroundColor: "rgb(31,32,34)", borderTopColor: "black" },
+        showLabel: false,
       }}
     >
       <Tabs.Screen name="Home" component={Home} />
-      <Tabs.Screen name="Movie" component={Movie} />
-      <Tabs.Screen name="TV" component={TV} />
       <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Folder" component={Folder} />
+      <Tabs.Screen name="User" component={User} />
     </Tabs.Navigator>
   );
 };
