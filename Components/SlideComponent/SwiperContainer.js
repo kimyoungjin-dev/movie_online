6465c1a3ee43ce3f9d619cba1f2b73d2c338599e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:36:34 +0900

    In the home component, make the slide part that encompasses the slide into a component.

---
+++ Components/SlideComponent/SwiperContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+import Swiper from "react-native-web-swiper";
+
+const { height: HEIGHT } = Dimensions.get("window");
+
+const Container = styled.View`
+  height: ${HEIGHT / 3};
+  width: 100%;
+`;
+
+const SwiperContainer = ({ children }) => {
+  return (
+    <Container>
+      <Swiper loop timeout={3} controlsEnabled={false}>
+        {children}
+      </Swiper>
+    </Container>
+  );
+};
+
+export default SwiperContainer;
