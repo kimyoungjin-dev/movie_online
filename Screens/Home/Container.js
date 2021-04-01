7d79f846ce4b87a858ff1e7ecf2a2a758f33ffe2
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-01 18:44:10 +0900

    Bring the api information to the container and pass it to the presenter.

--- Screens/Home/Container.js
+++ Screens/Home/Container.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MovieApi } from "../../Components/api";
 import Presenter from "./Presenter";
 
 const Container = () => {
-  return <Presenter />;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState();
+
+  const getData = async () => {
+    const [movieNowPlaying] = await MovieApi.MovieNowPlaying();
+    const [moviePopular] = await MovieApi.MoviePopular();
+    const [movieTopRated] = await MovieApi.MovieTopRated();
+    const [movieUpcoming] = await MovieApi.MovieUpcoming();
+    setMovies({
+      movieNowPlaying,
+      moviePopular,
+      movieTopRated,
+      movieUpcoming,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <Presenter {...movies} loading={loading} />;
 };
 
 export default Container;
