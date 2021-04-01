40bdce8862cc102859c8d30d2a7820ef3a933ee5
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:43:40 +0900

    Receives information from container.js, and renders movie information on the screen by recycling VerticalScroll and Vertical components.

--- Screens/Movie/Presenter.js
+++ Screens/Movie/Presenter.js
@@ -1,7 +1,151 @@
 import React from "react";
+import styled from "styled-components/native";
+import { ActivityIndicator, ScrollView } from "react-native";
+import VerticalScroll from "../../Components/SlideComponent/VerticalScroll";
+import Vertical from "../../Components/SlideComponent/Vertical";
+import ScrollContainer from "../../Components/SlideComponent/ScrollContainer";
 
-const Presenter = () => {
-  return <></>;
+const Container = styled.View`
+  padding: 0px 15px;
+`;
+
+const MoviePresenter = ({
+  Action,
+  Adventure,
+  Animation,
+  Comedy,
+  Crime,
+  Fantasy,
+  Horror,
+  Romance,
+  Thriller,
+  War,
+  loading,
+}) => {
+  return (
+    <ScrollContainer loading={loading}>
+      <Container>
+        <VerticalScroll title={"Action Movies"}>
+          {Action.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Adventure Movies"}>
+          {Adventure.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Animation Movies"}>
+          {Animation.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Comedy Movies"}>
+          {Comedy.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Crime Movies"}>
+          {Crime.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Fantasy Movies"}>
+          {Fantasy.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Horror Movies"}>
+          {Horror.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Romance Movies"}>
+          {Romance.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+
+        <VerticalScroll title={"Thriller Movies"}>
+          {Thriller.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+        <VerticalScroll title={"War Movies"}>
+          {War.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+      </Container>
+    </ScrollContainer>
+  );
 };
 
-export default Presenter;
+export default MoviePresenter;
