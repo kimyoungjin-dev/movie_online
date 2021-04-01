9def01a80ab9cbb03fe0ac148e41a36a8609b0a6
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:46:05 +0900

    Each logic is created to subdivide the presenter, display popular search terms before search, and search results appear on the screen when searching.

--- Screens/Search/Presenter.js
+++ Screens/Search/Presenter.js
@@ -1,7 +1,28 @@
 import React from "react";
+import ScrollContainer from "../../Components/SlideComponent/ScrollContainer";
+import SearchForm from "./SearchForm";
+import PreviousResults from "./PreviousResults";
+import AfterResults from "./AfterResults";
 
-const Presenter = () => {
-  return <></>;
+const Presenter = ({
+  onChange,
+  onSearch,
+  loading,
+  value,
+  movieResults,
+  tvResults,
+  moviePopular,
+  tvPopular,
+}) => {
+  return (
+    <ScrollContainer loading={loading}>
+      <SearchForm value={value} onSearch={onSearch} onChange={onChange} />
+      {movieResults.length === 0 && tvResults.length === 0 && (
+        <PreviousResults moviePopular={moviePopular} tvPopular={tvPopular} />
+      )}
+      <AfterResults movieResults={movieResults} tvResults={tvResults} />
+    </ScrollContainer>
+  );
 };
 
 export default Presenter;
