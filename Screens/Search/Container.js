7cc1d181b9462ac368f0dfc02d85be44a42a87c1
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:45:30 +0900

    Receive search information from api, store the value of input through useState, and pass option information for search to the presenter.

--- Screens/Search/Container.js
+++ Screens/Search/Container.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MovieApi, tvApi } from "../../Components/api";
+import Presenter from "./Presenter";
 
 const Container = () => {
-  return <></>;
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [searchData, setSearchData] = useState({
+    movieResults: [],
+    tvResults: [],
+  });
+  const [popularData, setPopularData] = useState({
+    moviePopular: [],
+    tvPopular: [],
+  });
+
+  const onChange = (text) => setValue(text);
+  const onSearch = async () => {
+    setLoading(true);
+    const [movieResults] = await MovieApi.MovieSearch(value);
+    const [tvResults] = await tvApi.showSearch(value);
+
+    setSearchData({
+      movieResults,
+      tvResults,
+    });
+    setLoading(false);
+  };
+
+  const getData = async () => {
+    const [moviePopular] = await MovieApi.MoviePopular();
+    const [tvPopular] = await tvApi.showPopular();
+
+    setPopularData({
+      moviePopular,
+      tvPopular,
+    });
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Presenter
+      onChange={onChange}
+      onSearch={onSearch}
+      loading={loading}
+      value={value}
+      {...searchData}
+      {...popularData}
+    />
+  );
 };
 
 export default Container;
