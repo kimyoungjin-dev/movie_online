bec94b6378beea8a07ff53f0a81955dc1e75b410
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:37:17 +0900

    By creating a scrollContainer, reusability is increased, and when not loading and when loading are separated.

---
+++ Components/SlideComponent/ScrollContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ActivityIndicator, ScrollView } from "react-native";
+
+const ScrollContainer = ({ loading, children }) => {
+  return (
+    <ScrollView
+      style={{
+        backgroundColor: "rgb(32,33,35)",
+        paddingLeft: 10,
+        paddingRight: 10,
+      }}
+      contentContainerStyle={{
+        flex: loading ? 1 : "auto",
+        justifyContent: loading ? "center" : "auto",
+      }}
+    >
+      {loading ? (
+        <ActivityIndicator color="white" size="large" />
+      ) : (
+        <>{children}</>
+      )}
+    </ScrollView>
+  );
+};
+
+export default ScrollContainer;
