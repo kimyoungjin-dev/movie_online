975d3715a189971450e349d3d2c54a5b9fd7b562
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 01:47:43 +0900

    The information that appears after searching is shown to the user, and the information is sent to presenter.js.

---
+++ Screens/Search/AfterResults.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Vertical from "../../Components/SlideComponent/Vertical";
+import VerticalScroll from "../../Components/SlideComponent/VerticalScroll";
+
+const AfterResults = ({ movieResults, tvResults }) => {
+  console.log(movieResults, tvResults);
+  return (
+    <>
+      {movieResults.length > 0 && (
+        <VerticalScroll title="Movies">
+          {movieResults.map((movie) => (
+            <Vertical
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              overview={movie.overview}
+              poster={movie.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+      )}
+      {tvResults.length > 0 && (
+        <VerticalScroll title="TV Program">
+          {tvResults.map((show) => (
+            <Vertical
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              overview={show.overview}
+              poster={show.poster_path}
+            />
+          ))}
+        </VerticalScroll>
+      )}
+    </>
+  );
+};
+
+export default AfterResults;
