183f30a072aff186e4df073ee88f803cc5e7a0d8
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 17:04:18 +0900

    In order to emphasize the readability of the code, components are managed by dividing each part of the screen into top, middle, and bottom.

--- Screens/Detail/Presenter.js
+++ Screens/Detail/Presenter.js
@@ -1,51 +1,25 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import styled from "styled-components/native";
 import ScrollContainer from "../../Components/SlideComponent/ScrollContainer";
 import Header from "./Header";
 import Center from "./Center";
-import { useNavigation } from "@react-navigation/native";
+import Bottom from "./Bottom";
+import Contents from "./Contents";
 
-const Overview = styled.Text`
-  opacity: 0.7;
-  color: white;
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  padding-bottom: 80px;
 `;
 
-const CastBox = styled.View`
-  flex-direction: row;
-`;
-
-const CastText = styled.Text`
-  color: white;
-  opacity: 0.8;
-`;
-
-const Presenter = ({ result, loading, cast, crew }) => {
-  const navigation = useNavigation();
+const Presenter = ({ result, loading, cast, crew, openBrowser }) => {
   return (
     <ScrollContainer loading={loading}>
-      <Header result={result} />
-      <Center />
-      <Overview>{result.overview}</Overview>
-      <View style={{ alignItems: "center", flexDirection: "row" }}>
-        <CastBox>
-          {cast?.map(
-            (c, index) =>
-              index < 2 && <CastText key={index}>{c.original_name}</CastText>
-          )}
-        </CastBox>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("Credit", {
-              cast,
-              crew,
-              loading,
-            })
-          }
-        >
-          <Text style={{ color: "white", opacity: 0.7 }}>...더보기</Text>
-        </TouchableOpacity>
-      </View>
+      <Container>
+        <Header result={result} openBrowser={openBrowser} />
+        <Center result={result} openBrowser={openBrowser} />
+        <Bottom cast={cast} crew={crew} result={result} />
+        <Contents result={result} />
+      </Container>
     </ScrollContainer>
   );
 };
