e7b0b3e1b5ee428723ff0455abcbaeaebdcddc2a
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-02 14:52:01 +0900

    Add Credit and Detail, and design headerTitleStyle.

--- Navigation/Stack.js
+++ Navigation/Stack.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Tabs from "./Tabs";
-import Movie from "../Screens/Movie";
-import TV from "../Screens/TV";
+import Credit from "../Components/Credit";
+import Detail from "../Screens/Detail";
 
 const Stack = createStackNavigator();
 
 export default () => {
   return (
     <Stack.Navigator
+      mode="modal"
       screenOptions={{
-        headerTitleAlign: "left",
+        headerTitleStyle: { fontSize: 16 },
+        headerTitleAlign: "center",
         headerStyle: {
           backgroundColor: "rgb(32,33,35)",
           borderBottomColor: "rgb(32,33,35)",
           shadowColor: "rgb(32,33,35)",
         },
         headerTintColor: "white",
+        headerBackTitleVisible: false,
       }}
     >
       <Stack.Screen name="Tabs" component={Tabs} />
-      <Stack.Screen name="Movie" component={Movie} />
-      <Stack.Screen name="TV" component={TV} />
+      <Stack.Screen name="Credit" component={Credit} />
+      <Stack.Screen name="Detail" component={Detail} />
     </Stack.Navigator>
   );
 };
